721b3db2343ce0df34db3624347aede97d3d8621
github username <github email address>
2020-09-13 21:54:23 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import GraceHopperQuoteComponent from './GraceHopperQuoteComponent.js'
+import CatComponent from './CatComponent.js'
+import MouseComponent from './MouseComponent.js'
 
 class App extends Component {
 	render() {
 		// your code in the return statement below!
 		return (
 			<div className="App">
-				<CatComponent />
 				<GraceHopperQuoteComponent />
-				{/* one more component missing */}
+				<CatComponent />
+				<MouseComponent />
 			</div>
 		);
 	}
